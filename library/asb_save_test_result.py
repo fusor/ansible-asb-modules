b42315e282ac6512e4d879abe2c6824c14f46a26
Shawn Hurley <shurley@redhat.com>
2017-08-16 15:33:20 -0400

    adding the save_test_result module.

---
+++ library/asb_save_test_result.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python
+
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+
+DOCUMENTATION = '''
+---
+module: asb_save_test_result
+short_description: Encodes the test results for testing action.
+description:
+     - Takes a test result object and makes it available during the testing
+     action.
+notes: []
+requirements: []
+author:
+    - "Red Hat, Inc."
+options:
+  fail:
+    description:
+      - 'Will signify whether the test result has passed or faild.'
+    default: false
+    type: bool
+  msg:
+    description:
+      - 'Determines the message th should be displayed for the failure or
+      success'
+    type: string
+'''
+
+EXAMPLES = '''
+- name: save test results
+  asb_save_test_result:
+    fail: true
+    msg: "Test failed"
+
+- name: test passed
+  asb_save_test_result:
+    msg: "Test passed!"
+'''
+RETURN = '''
+'''
+
+import json
+import base64
+from ansible.module_utils.basic import AnsibleModule
+
+TEST_RESULT_PATH = "/var/tmp/test-result"
+
+def main():
+
+    argument_spec = dict(
+        fail=dict(default=False, type='bool'),
+        msg=dict(default=None, type='str')
+    )
+
+    ansible_module = AnsibleModule(argument_spec=argument_spec)
+    try:
+        with open(TEST_RESULT_PATH, "w") as test_result_file:
+            # If failure is true then print the 1 code the test file.
+            if ansible_module.params['fail']:
+                test_result_file.write("1\n")
+            else:
+                # If failue is false, then 0 code signals success.
+                test_result_file.write("0\n")
+
+            if ansible_module.params['msg'] is not None:
+                test_result_file.write(ansible_module.params['msg'])
+    except Exception as error:
+        ansible_module.fail_json(msg="Error attempting to write binding: " + str(error))
+
+    ansible_module.exit_json(changed=True)
+
+
+if __name__ == '__main__':
+    main()
